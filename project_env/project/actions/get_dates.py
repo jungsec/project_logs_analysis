f395e6c7e3edaf851e5d50062ebb040d1f787eaf
Eddy Gasso <eddy6992@hotmail.fr>
2019-10-10 16:28:20 +0200

    get ip adress ~dates?

---
+++ project_env/project/actions/get_dates.py
@@ -0,0 +1,26 @@
+# coding : utf-8
+"""
+Name of the file : get_dates.py
+Description : the script get_dates will read and collect 
+the all the dates from a log file
+"""
+
+
+#Import libraries needed for the script
+
+#import argparse
+import os
+import re 
+import sys 
+
+
+__author__ = 'GMEddy'
+__copyright__ = ''
+__credits__ = ['GMEddy']
+__licence__ = ''
+__version__ = '0.0.1'
+__maintainer__ = 'GMEddy'
+__email__ = ''
+__status__ = 'Get_ip_log'
+
+#create a function for the dates in log file 
